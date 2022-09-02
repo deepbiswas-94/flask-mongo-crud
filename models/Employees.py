831189184ea52d8a10effee25ea028f222620fde
deepcoder12o <105338135+deepcoder12o@users.noreply.github.com>
2022-09-02 12:12:07 +0530

    Update Employees.py

--- models/Employees.py
+++ models/Employees.py
@@ -15,10 +15,18 @@ class Employees(db.Document):
     date_modified = db.DateTimeField(default=datetime.datetime.utcnow)
     
 def getEmployeesList():
+    """ Getting a list of employees
+        >>> Employees.objects()
+        :Returns:
+            - Return list of employees in JSON format
+    """     
     employees = Employees.objects()
     return jsonify(employees), 200    
 
 def hashPassword(function):
+    """ Generate Hash for a password
+        Modifies the normal password to hashed password
+    """         
     def wrapper(*args, **kwargs):
         password = args[0]['password']
         args[0]['password'] = bcryptObj.generate_password_hash(password)
@@ -27,25 +35,53 @@ def hashPassword(function):
 
 @hashPassword
 def createEmployee(data):
+    """ Insert a Employee document 
+        :Parameters:
+            - `data`:Dictionary containing insert data
+        :Returns:
+            - Returns result of insert operation
+    """    
     newEmployeeId = getEmployeeCount() + 1
     newEmployee = Employees(name=data['name'],email=data['email'],password=data['password'],employee_id=newEmployeeId)
     status = newEmployee.save(validate=True)
     return jsonify(status), 200
 
 def getSingleEmployee(data):
+    """ Get details of a single employee
+        :Parameters:
+            - `data`:Dictionary containing employee_id
+        :Returns:
+            - Returns detail of the employee
+    """        
     employees = Employees.objects.get_or_404(employee_id=data['employee_id'])
     return jsonify(employees), 200    
 
 def getEmployeeCount():
+    """ Getting number of employees
+        :Returns:
+            - Returns Number of employees
+    """        
     count = Employees.objects().count()
     return count
 
 def deleteEmployee(data):
+    """ Delete a Employee document 
+        :Parameters:
+            - `data`:Dictionary containing employee_id
+        :Returns:
+            - Return ID if the delete was successful
+    """            
     employee = Employees.objects.get_or_404(employee_id=data['employee_id'])
     employee.delete()
     return jsonify(str(employee.id)), 200
 
 def updateEmployee(data):
+    """ Update Single a Employee document 
+        :Parameters:
+            - `data`:Dictionary containing update data
+        :Returns:
+            - Return employee ID if successful
+    """    
     employees = Employees.objects.get_or_404(employee_id=data['employee_id'])
     employees.update(**data)
     return jsonify(str(employees.id)), 200
